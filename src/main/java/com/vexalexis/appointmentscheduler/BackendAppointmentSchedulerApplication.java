6afb3aac31554801ee9632626468c1671b74f7a2
Alexis Chavarria <joalexiscb@gmail.com>
2023-07-23 20:46:50 -0500

    Initial commit

---
+++ src/main/java/com/vexalexis/appointmentscheduler/BackendAppointmentSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.vexalexis.appointmentscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAppointmentSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAppointmentSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vexalexis/appointmentscheduler/BackendAppointmentSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vexalexis.appointmentscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAppointmentSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
